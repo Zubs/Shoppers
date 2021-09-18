f82744c4d866c5b7f4d11c5fa94da6fdca5ed14a
Zubs <zubairidrisaweda@gmail.com>
2021-09-18 12:19:20 +0100

    Fixed migrations

--- database/migrations/2021_07_09_222024_create_category_product_table.php
+++ database/migrations/2021_07_09_222024_create_category_product_table.php
@@ -15,8 +15,10 @@ class CreateCategoryProductTable extends Migration
     {
         Schema::create('category_product', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('product_id');
-            $table->foreignId('category_id');
+            $table->integer('product_id')->unsigned()->nullable();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->integer('category_id')->unsigned()->nullable();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
